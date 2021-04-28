7e8a33324278d6f4ddea088039c9d6d2afd1fdac
vanderL <vnd.vander@gmail.com>
2021-04-27 22:11:14 -0300

    Add repository with animation for button submit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaGithub, FaPlus } from 'react-icons/fa'
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api'
 
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-    return (
-        <Container>
-            <h1>
-                <FaGithub />
-                Repositorios
-            </h1>
+class Main extends Component {
+    state = {
+        repositories: [],
+        newRepo: '',
+        loading: false,
+    };
 
-            <Form>
-                <input type="text" placeholder="Adicionar repositórios"/>
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
 
-                <SubmitButton>
-                    <FaPlus />
-                </SubmitButton>
-            </Form>
-        </Container>
-  );
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const { newRepo, repositories } = this.state;
+
+        this.setState({ loading: true });
+
+        const response = await api.get(`/repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        this.setState({ 
+            repositories: [...repositories, data],
+            newRepo: '',
+            loading: false,
+        });
+    };
+
+
+    render() {
+        const { newRepo, loading } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGithub />
+                    Repositorios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="Adicionar repositórios"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <SubmitButton loading={loading}>
+                        {loading ? <FaSpinner color="#fff" size={14}/> : <FaPlus color="#fff" size={14}/>}
+                    </SubmitButton>
+                </Form>
+            </Container>
+        )
+    }
 }
 
 export default Main;
